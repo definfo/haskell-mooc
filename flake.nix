1fe11eb45a1a67e029a0b1825ff8815d88f5aacf
definfo <hjsdbb1@gmail.com>
2024-10-28 18:22:44 +0800

    feat: add nix-direnv support

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A Nix-flake-based Haskell development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default =
+        let
+          haskellPackages = pkgs.haskell.packages.ghc92;
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [ stack ] ++ (with haskellPackages; [
+            haskell-language-server
+            hlint
+          ]);
+        };
+      });
+    };
+}
