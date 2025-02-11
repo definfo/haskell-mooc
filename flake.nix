9ab3b8df39b4e3f3a64bb63bb85916b65fd740de
definfo <hjsdbb1@gmail.com>
2025-02-11 12:06:42 +0800

    chore: stack -> cabal, GHC 9.6.6 (LTS 22.43)
    NOTE:
    stack build      ===> cabal v2-build
    stack runHaskell ===> cabal v2-exec runHaskell
    stack ghci       ===> cabal v2-repl

--- flake.nix
+++ flake.nix
@@ -3,29 +3,50 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default =
-          let
-            ghc = pkgs.haskell.packages.ghc92.ghc;
-            hls = pkgs.haskell-language-server.override {
-              supportedGhcVersions = [ "92" ];
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            let
+              haskellPackages = pkgs.haskell.packages.ghc96;
+            in
+            pkgs.mkShell rec {
+              buildInputs =
+                (with haskellPackages; [
+                  ghc
+                  haskell-language-server
+                ])
+                ++ (with pkgs; [
+                  stack
+                  cabal-install
+                  hlint
+                  pkg-config
+                  zlib
+                ]);
+              # Ensure that libz.so and other libraries are available to TH
+              # splices, cabal repl, etc.
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
             };
-          in
-          pkgs.mkShell {
-            packages = [ ghc hls ] ++ (with pkgs; [
-              stack
-              cabal-install
-              hlint
-            ]);
-          };
-      });
+        }
+      );
     };
 }
